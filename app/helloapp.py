2faa92c29317e6af2af5802c86c1648dcf72f6ae
Riedwaan <riedwaanb@github.com>
2019-09-30 09:26:42 +0000

    minor update

--- app/helloapp.py
+++ app/helloapp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from flask import Flask,render_template,request
-import socket,random,os,time,argparse,shutil
+import socket,random,os,time,argparse
 
 app = Flask(__name__)
 
@@ -12,23 +12,25 @@ color_codes = {
     "pink": "#be2edd",
     "darkblue": "#130f40"
 }
-
 SUPPORTED_COLORS = ",".join(color_codes.keys())
 
 # Get color from Environment variable
 COLOR_FROM_ENV = os.environ.get('APP_COLOR')
+
 # Generate a random color
 COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink"])
 
+# default url just displays a hello from the host we are running on in the color we have
 @app.route("/")
 def main():
     # return 'Hello'
     return render_template('hello.html', name=socket.gethostname(), color=color_codes[COLOR])
 
-# url to call will be:
-# input?data=somevalue
+# Inputs some value into a log file and display a text box with the value in the color we have
+# url to call will be: input?data=somevalue
 @app.route('/input')
 def appinput():
+    # Retrieve the data
     inputdata = request.args.get('data')
 
     # If input is not what we expect do a normal hello
@@ -49,18 +51,18 @@ def appinput():
     f.write(host + ": " + now + " | " + COLOR + " | " + inputdata + "\n")
     f.close()
 
+    # Render with the data page
     return render_template('data.html', name=host, color=color_codes[COLOR], contents=inputdata)
 
-
 if __name__ == "__main__":
-
     print("Ready to say hello...")
 
-   # Check for Command Line Parameters for color
+    # Check for Command Line Parameters for color
     parser = argparse.ArgumentParser()
     parser.add_argument('--color', required=False)
     args = parser.parse_args()
 
+    # Determine the color for the page
     if args.color:
         print("Color from command line argument [" + args.color + "]\n")
         COLOR = args.color
@@ -81,5 +83,6 @@ if __name__ == "__main__":
     if not os.path.isdir("log"):
         os.mkdir("log")
 
-    # Run Flask Application
+    # Run the Flask Application
     app.run(host="0.0.0.0", port=8080)
+
