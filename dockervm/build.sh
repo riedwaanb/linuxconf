ce9511928e022182a3350a0683bb88e44d067516
Riedwaan Bassadien <riedwaab@microsoft.com>
2019-10-06 17:43:30 +0200

    fix cloudinit

--- dockervm/build.sh
+++ dockervm/build.sh
@@ -9,7 +9,7 @@ echo "$RESOURCEGROUP | $SERVERNAME | $DNSNAME"
 az group create -n $RESOURCEGROUP -l southafricanorth
 
 # Create a Linux VM and install Docker using cloudinit
-az vm create --name $SERVERNAME -l southafricanorth -g $RESOURCEGROUP --image Canonical:UbuntuServer:18.04-LTS:latest --public-ip-address-dns-name $SERVERNAME --public-ip-address-allocation static --size Standard_B2ms --custom-data cloud-init.yml --generate-ssh-keys
+az vm create --name $SERVERNAME -l southafricanorth -g $RESOURCEGROUP --image Canonical:UbuntuServer:18.04-LTS:latest --public-ip-address-dns-name $SERVERNAME --public-ip-address-allocation static --size Standard_B2ms --custom-data cloud-init.yaml --generate-ssh-keys
 
 # Open Port 8080 used in the demo
 az vm open-port --resource-group $RESOURCEGROUP --name $SERVERNAME --port 8080
